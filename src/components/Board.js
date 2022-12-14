401037a15a9a601c767d0f297dfb4a28b38450fa
Abdullah OZER <abdullahozer11@hotmail.com>
2022-12-14 16:35:23 +0100

    Header and Board components are created.

---
+++ src/components/Board.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+
+class Board extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+
+    }
+
+    render() {
+        return (
+            <div>
+                {/*todo add card for task board*/}
+            </div>
+        );
+    }
+}
+
+Board.propTypes = {};
+
+export default Board;
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+
+    }
+
+    render() {
+        return (
+            <div>
+                {/*todo add navbar*/}
+            </div>
+        );
+    }
+}
+
+Header.propTypes = {};
+
+export default Header;
