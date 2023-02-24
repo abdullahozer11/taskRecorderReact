c96225496192c69b59d072b46ab976034923e96c
Abdullah OZER <abdullahozer11@hotmail.com>
2023-02-24 22:16:25 +0100

    todo app is recoded.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
     return (
         <div className="App">
             <TaskList/>
+            <div className="footer">
+                <p>Created by <a href="http://www.instagram.com/aiwebtech" target="_blank">Abdullah Ozer</a></p>
+            </div>
         </div>
     );
 }
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,144 +1,70 @@
 import React, {Component} from "react";
-import {ListGroup} from 'react-bootstrap';
 
 import './TaskList.css';
 
 class TaskList extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
-            tasks: [
-                {
-                    id: 1,
-                    desc: "Vehicle needs serious cleaning including with sprays and sponges.",
-                    isCompleted: false,
-                },
-                {
-                    id: 2,
-                    desc: "Computer needs opening and dusting inside.",
-                    isCompleted: false,
-                },
-                {
-                    id: 3,
-                    desc: "Read a book (example: Dune 1965).",
-                    isCompleted: false,
-                },
-            ],
+            tasks: [],
             newTaskDescription: '',
         };
     };
 
     componentDidMount = () => {
-        // Add an event listener for keyboard events
         window.addEventListener('keydown', this.handleKeyDown);
     }
 
-    // Remove the event listener when the component unmounts
     componentWillUnmount = () => {
         window.removeEventListener('keydown', this.handleKeyDown);
     }
 
-    // This function will be called whenever a keyboard event occurs
     handleKeyDown = (event) => {
-        // Check if the key pressed was the enter key
         if (event.key === 'Enter') {
-            // Trigger the desired behavior for the button here
             this.addTask();
         }
     }
 
-    dragStart(event, id) {
-        // remember source tasks id
-        event.dataTransfer.setData('id', id);
-    };
-
-    dragOver(event) {
-        // prevent default in case of wrong drag action
-        event.preventDefault();
-    };
-
-    drop = (event, targetId) => {
-        // get initial task's id
-        const id = event.dataTransfer.getData('id');
-        // shallow copy the tasks list
-        const tasksCopy = [...this.state.tasks];
-        // find source index in order to get the task to move
-        const sourceIndex = tasksCopy.findIndex((item) => item.id === parseInt(id));
-        // find target index so that we can place the source task in the right place
-        const targetIndex = tasksCopy.findIndex((item) => item.id === targetId);
-        // splice the task from the source index
-        const [removed] = tasksCopy.splice(sourceIndex, 1);
-        // use again the splice method to inject our task in the right index
-        tasksCopy.splice(targetIndex, 0, removed);
-        // set state to finalize
+    deleteTask = (desc) => {
+        const copyTasks = [...this.state.tasks];
+        const index = copyTasks.findIndex((item) => item.desc === desc);
+        copyTasks.splice(index, 1);
         this.setState({
-            tasks: tasksCopy,
+            tasks: copyTasks,
         });
     };
 
-    deleteTask = (id) => {
-        // remove the task by filtering the tasks
-        const tasks = this.state.tasks.filter((t) => t.id !== id);
-        // set back the tasks in state
-        this.setState({tasks});
-    };
-
     handleSubmit = (event) => {
-        // prevent default form submission behavior
         event.preventDefault();
-
         this.addTask();
     };
 
     handleTaskDescriptionChange = (event) => {
-        // Update the value of the taskDescription state property with the value from the input field
         this.setState({newTaskDescription: event.target.value});
     };
 
-    toggleCheckbox(event, id) {
-        const element = event.target;
-        // Find the task with the specified id in the tasks list
-        const task = this.state.tasks.find(task => task.id === id);
-        // If the task was found, toggle its 'completed' property
-        if (task) {
-            task.isCompleted = !task.isCompleted;
-        }
-
-        // Find the closest parent element with the 'li' tag
-        const listItem = element.closest("li");
-        // Toggle the 'completed' class on the list item
-        listItem.classList.toggle('completed');
-
-        const sortedTasks = this.state.tasks.sort((a, b) => {
-            if (a.isCompleted && !b.isCompleted) {
-                return 1;
-            } else if (!a.isCompleted && b.isCompleted) {
-                return -1;
-            }
-            return 0;
-        });
+    toggleCheckbox(event, desc) {
+        const copyTasks = [...this.state.tasks];
+        console.log(copyTasks);
+        const index = copyTasks.findIndex((item) => item.desc === desc);
+        console.log(index);
+        copyTasks[index].isCompleted = event.target.checked;
         this.setState({
-            tasks: sortedTasks,
+            tasks: copyTasks,
         });
     };
 
     addTask = () => {
-        // shallow copy of the tasks list
-        const tasksCopy = [...this.state.tasks];
-        // check if new task description data are not empty
-        if (this.state.newTaskDescription.length) {
-            // create a new task object using the current state
+        const newTaskDescription = this.state.newTaskDescription;
+        if (newTaskDescription) {
             const newTask = {
-                id: tasksCopy.length + 1,
-                desc: this.state.newTaskDescription,
+                desc: newTaskDescription,
+                isCompleted: false,
             };
-
-            // add the new task to the tasks list
-            tasksCopy.unshift(newTask);
-            // update the state with the new tasks list
+            const copyTasks = [...this.state.tasks];
+            copyTasks.push(newTask);
             this.setState({
-                tasks: tasksCopy,
+                tasks: copyTasks,
                 newTaskDescription: '',
             });
         }
@@ -147,60 +73,39 @@ class TaskList extends Component {
     render() {
         const tasks = this.state.tasks;
         return (
-            <>
-                <div className="container">
-                    <div className="card px-3">
-                        <div className="card-body">
-                            <h4 className="card-title">Task List</h4>
-                            <div className="add-items d-flex">
-                                <input type="text"
-                                       className="form-control todo-list-input"
-                                       placeholder="What do you need to do today?"
-                                       onChange={this.handleTaskDescriptionChange}
-                                       value={this.state.newTaskDescription}
-                                />
-                                <button
-                                    onClick={this.handleSubmit}
-                                    className="add btn btn-primary font-weight-bold todo-list-add-btn">Add
-                                </button>
-                            </div>
-                            <div className="list-wrapper">
-                                <ul className="d-flex flex-column-reverse todo-list">
-                                    <ListGroup>
-                                        {tasks.map((task) => (
-                                            <ListGroup.Item
-                                                key={task.id}
-                                                draggable
-                                                onDragStart={(event) => this.dragStart(event, task.id)}
-                                                onDragOver={this.dragOver}
-                                                onDrop={(event) => this.drop(event, task.id)}
-                                            >
-                                                <li key={task.id}>
-                                                    <div className="form-check">
-                                                        <label className="form-check-label">
-                                                            <input className="checkbox"
-                                                                   type="checkbox"
-                                                                   onChange={(event) => this.toggleCheckbox(event, task.id)}
-                                                            />
-                                                            {task.desc}
-                                                            <i className="input-helper"></i></label>
-                                                    </div>
-                                                    <div className="button-wrapper">
-                                                        <i onClick={() => this.deleteTask(task.id)}
-                                                           className="fa fa-trash fa-lg delete-task-button"
-                                                           aria-hidden="true">
-                                                        </i>
-                                                    </div>
-                                                </li>
-                                            </ListGroup.Item>
-                                        ))}
-                                    </ListGroup>
-                                </ul>
-                            </div>
-                        </div>
+            <div className="container">
+                <div className="header">
+                    <h1>Todo List</h1>
+                </div>
+                <div className="content">
+                    <div className="input">
+                        <input type="text" id="input" value={this.state.newTaskDescription}
+                               onChange={this.handleTaskDescriptionChange}
+                               placeholder="Enter your todo"></input>
+                        <button id="add" onClick={this.addTask}>Add</button>
+                    </div>
+                    <div className="todo-list">
+                        <ul id="todo-list">
+                            {tasks.map((task) => (
+                                <li key={task.desc} draggable="true"
+                                    onDragStart={(event) => this.dragStart(event, task.desc)}
+                                    onDragOver={(event) => this.dragOver(event)}
+                                    onDrop={(event) => this.drop(event, task.desc)}>
+                                    <input type="checkbox" id={task.desc}
+                                           onChange={(event) => this.toggleCheckbox(event, task.desc)}></input>
+                                    <label htmlFor={task.desc}>{task.desc}</label>
+                                    <a className="trash-icon" href="#" onClick={() => this.deleteTask(task.desc)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+                                            <path
+                                                d="M 21 2 C 19.354545 2 18 3.3545455 18 5 L 18 7 L 10.154297 7 A 1.0001 1.0001 0 0 0 9.984375 6.9863281 A 1.0001 1.0001 0 0 0 9.8398438 7 L 8 7 A 1.0001 1.0001 0 1 0 8 9 L 9 9 L 9 45 C 9 46.645455 10.354545 48 12 48 L 38 48 C 39.645455 48 41 46.645455 41 45 L 41 9 L 42 9 A 1.0001 1.0001 0 1 0 42 7 L 40.167969 7 A 1.0001 1.0001 0 0 0 39.841797 7 L 32 7 L 32 5 C 32 3.3545455 30.645455 2 29 2 L 21 2 z M 21 4 L 29 4 C 29.554545 4 30 4.4454545 30 5 L 30 7 L 20 7 L 20 5 C 20 4.4454545 20.445455 4 21 4 z M 11 9 L 18.832031 9 A 1.0001 1.0001 0 0 0 19.158203 9 L 30.832031 9 A 1.0001 1.0001 0 0 0 31.158203 9 L 39 9 L 39 45 C 39 45.554545 38.554545 46 38 46 L 12 46 C 11.445455 46 11 45.554545 11 45 L 11 9 z M 18.984375 13.986328 A 1.0001 1.0001 0 0 0 18 15 L 18 40 A 1.0001 1.0001 0 1 0 20 40 L 20 15 A 1.0001 1.0001 0 0 0 18.984375 13.986328 z M 24.984375 13.986328 A 1.0001 1.0001 0 0 0 24 15 L 24 40 A 1.0001 1.0001 0 1 0 26 40 L 26 15 A 1.0001 1.0001 0 0 0 24.984375 13.986328 z M 30.984375 13.986328 A 1.0001 1.0001 0 0 0 30 15 L 30 40 A 1.0001 1.0001 0 1 0 32 40 L 32 15 A 1.0001 1.0001 0 0 0 30.984375 13.986328 z"/>
+                                        </svg>
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
-            </>
+            </div>
         );
     };
 }
